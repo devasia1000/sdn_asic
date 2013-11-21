4aa1f3c4fc56acb66ce84f8861234536841b65de
Devasia Manuel <devasia1000@gmail.com>
2013-11-20 20:21:20 -0600

    added dump commands

--- main.v
+++ main.v
@@ -7,29 +7,44 @@ module test_bench;
 
     chained_comp uut(test_in0, test_in1, test_out);
 
-    initial
-    begin
+    initial begin
 
+        // these dump commands don't seem to be helping
+        $dumpfile("main.vcd");
+        $dumpvars(0, test_in0, test_in1, test_out);
+
+        $dumpall;
+        
         // test vector 1
         test_in0 = 2'b00;
         test_in1 = 2'b00;
         # 200;
+
+        $dumpall;
 
         // test vector 2
         test_in0 = 2'b01;
         test_in1 = 2'b00;
         # 200;
 
+        $dumpall;
+        
         // test vector 3
         test_in0 = 2'b01;
         test_in1 = 2'b11;
         # 200;
         
+        $dumpall;
+        
         // test vector 4
         test_in0 = 2'b10;
         test_in1 = 2'b10;
         # 200;
-
+        
+        $dumpall;
+        
+        $dumpflush;
+        
         $stop;
     
     end
