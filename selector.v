f5bb55f16bc0cbb6eda0b36edda31b1a2eb4a605
Devasia Manuel <devasia1000@gmail.com>
2013-11-23 14:31:54 -0600

    cleaned up selector code

--- selector.v
+++ selector.v
@@ -1,24 +1,14 @@
-/*module selector
-
-# (parameter N = 4)
+// selects specific bits of a packet
+module selector
+// inputs: SIZE is the size of the packet in bits
+//         START is the starting index to grab bits
+//         END is the end index to grab bits
+#(parameter SIZE = 2, parameter START = 2, parameter END = 2)
 (
-    input wire [N-1 : 0] a, b,
-    output wire [N-1 : 0] sum,
-    output wire cout
+    input wire [SIZE - 1 : 0]  packet,
+    output wire [END - START : 0]  out
 );
 
-endmodule*/
+assign out = packet [END : START];
 
- // selects specific bits of a packet
- module selector
- // inputs: SIZE is the size of the packet in bits
- //         START is the starting index to grab bits
- //         END is the end index to grab bits
- #(parameter SIZE = 2, parameter START = 2, parameter END = 2)
- (
-       input wire [SIZE - 1 : 0]  packet,
-       output wire [END - START : 0]  out
- );
-
- assign out = packet [END : START];
- endmodule
+endmodule
